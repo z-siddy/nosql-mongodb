c9987b9fd05c654815f2d5d2bae2adb9adeda68b
Žygimantas <zygimantas@seemsneat.com>
2019-12-03 20:37:01 +0200

    fully working code

--- routes/teams.js
+++ routes/teams.js
@@ -1,83 +1,97 @@
-const express = require('express')
-const router = express.Router()
-const Team = require('../models/team')
-const Player = require('../models/player')
+const express = require("express");
+const router = express.Router();
+const Team = require("../models/team");
+const Player = require("../models/player");
+var ObjectId = require('mongoose').Types.ObjectId;
 
 //get all players
 async function getAllPlayers(req, res) {
   try {
-    const teams = await Team.find({}, 'players');
+    const teams = await Team.find({}, "players");
     const players = [];
 
     teams.map(tm => players.push(tm.players));
-
     return res.status(201).json({ players: players });
   } catch (err) {
     return res.status(400).json({ message: err });
   }
-};
+}
 
 async function getTeam(req, res, next) {
   try {
-    team = await Team.findById(req.params.id)
+    team = await Team.findById(req.params.id);
     if (team == null) {
-      return res.status(404).json({ message: 'Cant find the specified team'})
+      return res.status(404).json({ message: "Cant find the specified team" });
     }
-  } catch(err){
-    return res.status(500).json({ message: err.message })
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
 
-  res.team = team
-  next()
+  res.team = team;
+  next();
 }
 
-router.get('/players', getAllPlayers);
+router.get("/players", getAllPlayers);
 
 // Get all teams
-router.get('/', async(req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const teams = await Team.find()
-    res.json(teams)
-  } catch(err) {
-    res.status(500).json({ message: err.message })
+    const teams = await Team.find();
+    res.json(teams);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
-router.get('/countTeams', async(req, res) => {
+router.get("/countTeams", async (req, res) => {
   try {
-    Team.countDocuments({}, (err, count) =>
-      res.json(count)
-    );
-  } catch(err) {
-    res.status(500).json({ message: res.message })
+    Team.countDocuments({}, (err, count) => res.json(count));
+  } catch (err) {
+    res.status(500).json({ message: res.message });
   }
-})
+});
 
 // Get one team
-router.get('/:id', getTeam, (req, res) => {
-  res.json(res.team)
-})
+router.get("/:id", getTeam, (req, res) => {
+  res.json(res.team);
+});
+
+//Get a the average of a team's player age
+router.get("/:id/avg", async (req, res) => {
+  const tid = req.params.id;
+
+  Team.aggregate([
+    { "$match": { "_id": new ObjectId(tid) } },
+    { "$unwind": "$players" },
+    { "$group": { "_id": "null", "avg" : { "$avg": "$players.age" } } }],
+    function( err, data ) {
+      if ( err )
+        throw err;
+        res.status(200).json({data});
+    }
+  )
+});
 
 // Add a player to a specific team
-router.post('/:id/addPlayer', getTeam, async (req, res) => {
+router.post("/:id/addPlayer", getTeam, async (req, res) => {
   try {
     const newPlayer = new Player({
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       age: req.body.age,
       number: req.body.number
-    })
-    const team = res.team
-    team.players.push(newPlayer)
-    await team.save()
-    res.status(201).json(team)
-  } catch(err) {
-    res.status(400).json({ message: err.message })
+    });
+    const team = res.team;
+    team.players.push(newPlayer);
+    await team.save();
+    res.status(201).json(team);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 // Create one team
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const team = new Team({
     name: req.body.name,
     city: req.body.city,
@@ -85,50 +99,50 @@ router.post('/', async (req, res) => {
       country: req.body.info.country,
       divisionID: req.body.info.divisionID
     }
-  })
+  });
 
   try {
-    const newTeam = await team.save()
-    res.status(201).json(newTeam)
-  } catch(err) {
-    res.status(400).json({ message: err.message })
+    const newTeam = await team.save();
+    res.status(201).json(newTeam);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 // Update one team
-router.patch('/:id', getTeam, async (req, res) => {
+router.patch("/:id", getTeam, async (req, res) => {
   if (req.body.name != null) {
-    res.team.name = req.body.name
+    res.team.name = req.body.name;
   }
   if (req.body.city != null) {
-    res.team.city = req.body.city
+    res.team.city = req.body.city;
   }
   if (req.body.info.divisionID != null) {
-    res.team.info.divisionID = req.body.info.divisionID
+    res.team.info.divisionID = req.body.info.divisionID;
   }
   if (req.body.info.country != null) {
-    res.team.info.country = req.body.info.country
+    res.team.info.country = req.body.info.country;
   }
   if (req.body.players != null) {
-    res.team.players = req.body.players
+    res.team.players = req.body.players;
   }
   try {
-    const updatedTeam = await res.team.save()
-    res.json(updatedTeam)
+    const updatedTeam = await res.team.save();
+    res.json(updatedTeam);
   } catch {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 // Delete one team
-router.delete('/:id', getTeam, async (req, res) => {
+router.delete("/:id", getTeam, async (req, res) => {
   try {
-    const name = res.team.name
-    await res.team.remove()
-    res.json({ message: 'Deleted '+ name })
-  } catch(err) {
-    res.status(500).json({ message: err.message })
+    const name = res.team.name;
+    await res.team.remove();
+    res.json({ message: "Deleted " + name });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
